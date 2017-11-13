978e65582fa114783f71743bfdcdf42a7d29d3f2
Nargonath <jonas.pauthier@gmail.com>
2017-11-14 00:49:03 +0100

    Corrected gh60 spaceFn keymapping

--- keyboard/gh60/keymap_ngt.c
+++ keyboard/gh60/keymap_ngt.c
@@ -15,15 +15,15 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Shift| <|  W|  X|  C|  V|  B|  N|  ,|  ;|  :|  !|Shift     |
      * |-----------------------------------------------------------|
-     * |    |Gui |Alt |      Space             |Alt |Gui |App |    |
+     * |    |Gui |Alt |      Space                  |Alt |App |    |
      * `-----------------------------------------------------------'
      */
     KEYMAP(
         ESC, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, MINS, EQL, BSPC,
-        TAB, A, Z, E, R, T, Y, U, I, O, P, LBRC, RBRC, BSLS,
-        CAPS, Q, S, D, F, G, H, J, K, L, M, QUOT, TRNS, ENT,
-        LSFT, TRNS, Z, X, C, V, B, N, COMM, SCLN, DOT, SLSH, TRNS, RSFT,
-        TRNS, LGUI, LALT, FN0, RALT, RGUI, APP, TRNS, TRNS),
+        TAB, Q, W, E, R, T, Y, U, I, O, P, LBRC, RBRC, BSLS,
+        LCTL, A, S, D, F, G, H, J, K, L, SCLN, QUOT, TRNS, ENT,
+        LSFT, NUBS, Z, X, C, V, B, N, M, COMM, DOT, SLSH, TRNS, RSFT,
+        TRNS, LGUI, LALT, FN0, TRNS, TRNS, RALT, APP, TRNS),
 
     /* Overlay 1: SpaceFN
      * ,-----------------------------------------------------------.
@@ -41,7 +41,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(
         GRV, F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12, DEL,
         TRNS, TRNS, MUTE, VOLD, VOLU, TRNS, TRNS, HOME, UP, END, PSCR, SLCK, PAUS, INS,
-        TRNS, MSTP, MPLY, MPRV, MNXT, TRNS, PGUP, LEFT, DOWN, RGHT, TRNS, TRNS, TRNS, TRNS,
+        CAPS, MSTP, MPLY, MPRV, MNXT, TRNS, PGUP, LEFT, DOWN, RGHT, TRNS, TRNS, TRNS, TRNS,
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, SPC, PGDN, GRV, FN1, TRNS, TRNS, TRNS, TRNS,
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS),
 };
